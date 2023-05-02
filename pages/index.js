5fb9d057a549dfc6f9b7b3101c0d1473b75d0fa4
Anthony <ge01020g@gmail.com>
2023-05-02 17:00:24 -0400

    rofl

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import About from './query_input.js'
+import Output from './output.js'
 import Script from 'next/script'
 
 const Home = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center bg-gradient-to-r from-blue-900 to-purple-900'>
       <Script src="model.js" />
-      <h1 className='top-20 bottom-[20%] font-bold text-9xl text-green-500'>
-        Ello Govna
-      </h1>
+      <div className='relative top-[10px] mb-10 text-blue-200 p-3 w-[50%] 
+      ml-auto mr-auto bg-black bg-opacity-60 shadow-lg shadow-black'>
+        <h1 className='font-bold text-[400%] w-[80%] ml-auto mr-auto'>
+          the best search engine ever
+        </h1>
+        <p className='mt-2'>Our existence as a human species was a gift by God!</p>
+        <br></br>
+        <img src='images/jason.jpg' className='ml-auto mr-auto max-w-[400%] transition duration-300 ease-in-out hover:shadow-lg hover:shadow-white'
+        alt='our beloved founder'/>
+        <p className='text-1xl'>our beloved founder</p>
+      </div>
       <About></About>
-      <p className='text-blue-400'> bro</p>
+      <Output content={['hamburger', 'cheeseburger', 'big mac', 'whopper']}></Output>
+      <br></br>
     </div>
   )
 }
--- pages/query_input.js
+++ pages/query_input.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 
 class query_input extends Component{
+
     constructor() {
         super();
         this.state = {
-            textAreaValue:  ""
+            textAreaValue:  "",
+            show: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     };
 
     handleChange(e) {
-        this.setState({ textAreaValue: event.target.value})
+        this.setState({ 
+            textAreaValue: event.target.value,
+        })
     }
     
     handleSubmit(e) {
-        alert('hello: ' + this.state.textAreaValue);
+        alert('eeee: ' + this.state.textAreaValue);
+        this.setState({show: true})
         event.preventDefault();
     }
 
@@ -23,20 +28,23 @@ class query_input extends Component{
     render(){
         return (
             <div>
-                <div className='top-[40em] ml-[10%] mr-[10%] 
-                text-2xl font-bold bg-slate-400 p-8 rounded-xl bg-opacity-20'>
+                <div className='top-[10em] ml-[10%] mr-[10%] bg-blue-200 p-8 rounded-xl bg-opacity-60'>
                     <form onSubmit={this.handleSubmit}>
                         <label className='text-left'>
-                            Put your stuff in here:
+                            <p className="text-[150%] text-bold text-black">Put your stuff in here: </p>
                             <textarea className='overflow-scroll bg-white w-[100%] text-left
-                            h-[400px] mt-2' 
+                            h-[400px] mt-2 bg-opacity-50 text-[150%]' 
                             id="query"
                                 value={this.state.textAreaValue}
                                 onChange={this.handleChange}
                             />
+                            <p className="text-2xl">Insert keywords below: </p>
+                            <div class="input-area" id="keywords" className="w-[100%]">
+                                <input type="text" id="key-input" className="w-[100%]"/>
+                            </div>
                         </label>
                         <button type="submit" value="Submit" className='align-center bg-green-400 p-10 rounded-xl
-                        mt-5'>push</button>
+                        mt-5 bg-opacity-90'>PUSH!</button>
                     </form>
                 </div>
             </div>
