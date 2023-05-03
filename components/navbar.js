9e7f964ff8313f7c995b6624e336bd4554fc9c2a
Anthony <ge01020g@gmail.com>
2023-05-03 01:38:07 -0400

    about page and navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,5 @@
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';  
 
@@ -9,19 +8,12 @@ export default function navbar(){
         <div>
             <Navbar className='sticky bg-white' expand="md">
                 <Container>
-                    <Navbar.Brand href="index">balls</Navbar.Brand>  
+                    <Navbar.Brand href="index">SwapWikiMachine</Navbar.Brand>  
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />  
                     <Navbar.Collapse id="basic-navbar-nav">  
                         <Nav className="me-auto">  
-                        <Nav.Link href="#home">Home</Nav.Link>  
-                        <Nav.Link href="#link">Link</Nav.Link>  
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">  
-                            <NavDropdown.Item href="#action/3.1">Dropdown Item 1</NavDropdown.Item>  
-                            <NavDropdown.Item href="#action/3.2">Dropdown Item 2</NavDropdown.Item>  
-                            <NavDropdown.Item href="#action/3.3">Dropdown Item 3</NavDropdown.Item>  
-                            <NavDropdown.Divider />  
-                            <NavDropdown.Item href="#action/3.4">Another Item</NavDropdown.Item>  
-                        </NavDropdown>  
+                        <Nav.Link href="index">Home</Nav.Link>  
+                        <Nav.Link href="about">About</Nav.Link>
                         </Nav>  
                     </Navbar.Collapse>
                 </Container>
