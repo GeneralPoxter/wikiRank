ddb045df0a9bca34f737d24814d94b6a30cdf5bc
Anthony <ge01020g@gmail.com>
2023-05-03 00:39:23 -0400

    added bootstrap

--- components/query_input.js
+++ components/query_input.js
@@ -32,21 +32,19 @@ export default class query_input extends Component {
             <div>
                 <div className='top-[10em] ml-[10%] mr-[10%] bg-blue-200 p-8 rounded-xl bg-opacity-60'>
                     <form onSubmit={this.handleSubmit}>
-                        <label className='text-left'>
-                            <p className="text-[150%] text-bold text-black">Put your stuff in here: </p>
-                            <textarea className='overflow-scroll bg-white w-[100%] text-left
-                            h-[400px] mt-2 bg-opacity-50 text-[150%]'
-                                id="query"
-                                value={this.state.textAreaValue}
-                                onChange={this.handleChange}
-                            />
-                            <p className="text-2xl">Insert keywords below: </p>
-                            <div className="input-area w-[100%]" id="keywords">
-                                <input type="text" id="key-input" className="w-[100%]" />
-                            </div>
-                        </label>
+                        <p className="text-[150%] text-bold text-black text-left">Put your stuff in here: </p>
+                        <textarea className='overflow-scroll bg-white w-[100%] text-left
+                        h-[400px] mb-2 bg-opacity-50 text-[150%]'
+                            id="query"
+                            value={this.state.textAreaValue}
+                            onChange={this.handleChange}
+                        />
+                        <p className="text-2xl text-left">Insert keywords below: </p>
+                        <div className="input-area w-[100%]" id="keywords">
+                            <input type="text" id="key-input" className="w-[100%]" />
+                        </div>
                         <button type="submit" value="Submit" className='align-center bg-green-400 p-10 rounded-xl
-                        mt-5 bg-opacity-90'>PUSH!</button>
+                        mt-4 bg-opacity-90'>PUSH!</button>
                     </form>
                 </div>
             </div>
