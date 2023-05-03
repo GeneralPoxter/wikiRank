757dbf72ef743758d84d3f4ad6828a03807f2e56
Charles Wang <charleszw16@gmail.com>
2023-05-03 17:20:31 -0400

    header changes

--- components/header.js
+++ components/header.js
@@ -5,10 +5,15 @@ export default function Header() {
 
     return (
         <div className='bg-[#181825] w-screen mb-[5%] text-[#cdd6f4] p-8'>
-            <h1 className='flex justify-center font-bold text-[5em]'>wikiRank</h1>
+            <div className="flex justify-center space-x-[2rem]">
+                <h1 className='font-bold text-[5rem]'>wikiRank</h1>
+                <img src="logo.svg" className="w-[5rem]" />
+            </div>
+
             <p className="flex justify-center italic">quickly parse any complicated document for easy answers</p>
             <div className="flex justify-center mt-[2em]">
                 <a href="/"><button className={buttonStyle}>Home</button></a>
+
                 <a href="/about"><button className={buttonStyle}>About</button></a>
             </div>
         </div>
