a03084b0df3d9288939fd58a967ae0853b28cb9b
Charles Wang <charleszw16@gmail.com>
2023-05-03 14:15:19 -0400

    couple more results changes
    will add back deleted features later anthony my bad

--- components/input.js
+++ components/input.js
@@ -55,7 +55,7 @@ export default function Input() {
                     <div>
                         <div className="text-[200%] text-[#cdd6f4] flex justify-between">
                             <div>
-                                <a href={article.url} target='_target' className='underline text-[#89b4fa]'>
+                                <a href={article.url} target='_blank' className='underline text-[#89b4fa]'>
                                     {article.title}
                                 </a>
                             </div>
@@ -63,24 +63,21 @@ export default function Input() {
                         </div>
 
                         <div className='mt-[1em] mb-[3em] text-[#cdd6f4] bg-[#585b70] 
-                            p-[2em] rounded-xl flex justify-center'>
-                            <div className="w-[50%]">
-                                <div className='overflow-scroll h-[300px] text-[#cdd6f4]'>
-                                    <p>{article.summary}</p>
-                                </div>
+                            p-[2em] rounded-xl flex justify-between'>
+                            <div className="overflow-scroll text-[1.3em] text-left w-2/3 h-[300px] text-[#cdd6f4]">
+                                {article.summary}
                             </div>
-                            <div
-                                className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat ml-8"
-                                data-te-ripple-init
-                                data-te-ripple-color="light">
-                                <img
-                                    src={article.image}
-                                    className="max-h-[15em] w-auto"
-                                />
-                                <a href={article.url}>
+                            <div>
+                                <a href={article.url} target="_blank">
+                                    <img
+                                        src={article.image}
+                                        className="max-h-[15em]"
+                                    />
                                     <div
-                                        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                                        className=""></div>
                                 </a>
+
+
                             </div>
                         </div>
                     </div>
