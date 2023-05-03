5f875a0f37e76334001985a922895e136bbc3e14
Charles Wang <charleszw16@gmail.com>
2023-05-03 17:31:54 -0400

    make footer nicer :)

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 export default function Header() {
-    const linkStyle = "underline text-[#89b4fa]";
-
     return (
         <div className='bg-[#181825] w-screen mt-[5%] text-[#cdd6f4] p-8 text-center'>
-            made with 💜 by <a href="https://github.com/geant04" target="_blank" className={linkStyle}>Anthony Ge</a>, <a href="https://github.com/aczw" target="_blank" className={linkStyle}>Charles Wang</a>, <a href="https://github.com/GeneralPoxter/wikiRank" target="_blank" className={linkStyle}>Jason Liu</a>
+            made with 💜 by Anthony Ge, Charles Wang, and Jason Liu. source code is available on <a href="https://github.com/GeneralPoxter/wikiRank" target="_blank" className="underline text-[#89b4fa]">GitHub</a>!
         </div>
     );
 }
